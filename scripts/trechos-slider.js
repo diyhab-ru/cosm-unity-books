d915b9709dcb5b9126ea3116c2524aa9c97fafc3
Anatoly Zhuravlev <newstreetpunk@yandex.ru>
2026-04-02 15:31:51 +0500

    techos slider

---
+++ scripts/trechos-slider.js
@@ -0,0 +1,24 @@
+const trechosSlider = new Swiper('.trechos-wrapper', {
+    loop: false,
+    navigation: {
+        nextEl: '.trechos-slider-next',
+        prevEl: '.trechos-slider-prev',
+    },
+    pagination: {
+        el: '.trechos-slider-pagination',
+        type: 'bullets',
+        clickable: true,
+    },
+    breakpoints: {  
+        '320': {
+            slidesPerView: 1,
+            slidesPerGroup: 1,
+            spaceBetween: 30,
+        },
+        '993': { 
+            slidesPerView: 2,
+            slidesPerGroup: 2,
+            spaceBetween: 54,
+        },
+    },
+});
